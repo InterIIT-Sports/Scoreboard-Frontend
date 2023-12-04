58d57dcdada7e52db31375f043123229d49f7f18
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2023-12-04 14:31:12 +0530

    implemented functionality to export schedule data

--- src/Screens/Admin/ScheduleEditor.tsx
+++ src/Screens/Admin/ScheduleEditor.tsx
@@ -7,32 +7,77 @@ import {
 	DropdownCellType,
 	DateCellType,
 } from "handsontable/cellTypes";
+import { registerPlugin, ExportFile } from "handsontable/plugins";
 import EventCatagories from "../../types/EventCategories";
 import { Team } from "../../types/Team";
+import { useEffect, useRef } from "react";
 
 registerCellType(TimeCellType);
 registerCellType(DropdownCellType);
 registerCellType(DateCellType);
 
+registerPlugin(ExportFile);
+
 const ScheduleEditor = ({ teams }: { teams: Team[] }) => {
+	const hotRef = useRef<HotTable | null>(null);
+
+	const buttonClickCallback = useRef<() => void>();
+
+	useEffect(() => {
+		const hot = hotRef?.current?.hotInstance;
+		const exportPlugin = hot!.getPlugin("exportFile");
+
+		buttonClickCallback.current = async () => {
+			const exportedBlob = exportPlugin!.exportAsBlob("csv", {
+				bom: false,
+				columnDelimiter: ",",
+				columnHeaders: false,
+				exportHiddenColumns: true,
+				exportHiddenRows: true,
+				mimeType: "text/csv",
+				rowDelimiter: "\r\n",
+				rowHeaders: true,
+			});
+
+			console.log(await exportedBlob.text());
+		};
+	}, []);
+
 	return (
 		<div className="usersContainer">
 			<div className="top">Schedule Table</div>
-			<HotTable
+			<button
+				className="styledButton"
 				style={{ marginTop: "5px" }}
-				data={[["", "", "", "", "", ""]]}
+				onClick={buttonClickCallback.current}
+			>
+				Save
+			</button>
+
+			<HotTable
+				ref={hotRef}
+				style={{ marginTop: "5px" }}
 				rowHeaders={true}
 				columns={[
 					{ type: "dropdown", source: Object.values(EventCatagories) },
+					{ type: "text" },
 					{ type: "text" },
 					{ type: "date", correctFormat: true },
 					{ type: "time", timeFormat: "h:mm:ss a", correctFormat: true },
 					{ type: "dropdown", source: teams.map((team) => team.name) },
 					{ type: "dropdown", source: teams.map((team) => team.name) },
 				]}
-				colHeaders={["Event", "Name", "Date", "Start Time", "Team 1", "Team 2"]}
+				colHeaders={[
+					"Event",
+					"Name",
+					"Subtite",
+					"Date",
+					"Start Time",
+					"Team 1",
+					"Team 2",
+				]}
 				minSpareRows={1}
-				colWidths={[150, 200, 100, 100, 150, 150]}
+				colWidths={[150, 150, 150, 100, 100, 150, 150]}
 				licenseKey="non-commercial-and-evaluation" // for non-commercial use only
 			/>
 		</div>
