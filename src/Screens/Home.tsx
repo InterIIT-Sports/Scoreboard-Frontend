678c291245d11f256bd12cbd5dd17c70846dd8cd
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2023-12-09 21:46:22 +0530

    changed format of homepage and fixed css

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -88,24 +88,26 @@ const Home = () => {
 						<Header />
 					</div>
 					<div className="content">
-						<div className="leftContainer">
-							<LiveEventsViewer
-								onScoreUpdate={updateScoreOfEvent}
-								liveEvents={liveEvents}
-							/>
-							<UpcomingEventsViewer
-								heading={
-									<h3
-										style={{ marginTop: "0", marginBlockEnd: "0.5em" }}
-										className="wire"
-									>
-										Upcoming Events
-									</h3>
-								}
-								events={upcomingEvents}
-							/>
+						<LiveEventsViewer
+							onScoreUpdate={updateScoreOfEvent}
+							liveEvents={liveEvents}
+						/>
+						<div className="bottomContainer">
+							<div className="leftContainer">
+								<UpcomingEventsViewer
+									heading={
+										<h3
+											style={{ marginTop: "0", marginBlockEnd: "0.5em" }}
+											className="wire"
+										>
+											Upcoming Events
+										</h3>
+									}
+									events={upcomingEvents}
+								/>
+							</div>
+							<PastGamesResultsViewer events={pastEvents} />
 						</div>
-						<PastGamesResultsViewer events={pastEvents} />
 					</div>
 					<Footer />
 				</>
