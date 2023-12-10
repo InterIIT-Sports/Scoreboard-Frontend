9e215e70e8eb8630e1b6596e03d247f7ac030ff8
Karan Gandhi <karangandhi.programming@gmail.com>
2023-12-10 17:02:01 +0530

    completed athletic-event-logs

--- src/Screens/Admin/EditScores.tsx
+++ src/Screens/Admin/EditScores.tsx
@@ -19,161 +19,144 @@ import AthleticsEvent from "../../types/AthleticsEvent";
 const EVENT_START_BUFFER = 15 * 60 * 1000; //in milliseconds
 
 const EditScores = () => {
-	const getAccessToken = useAuthHeader();
-	const setToast = useContext(ToastContext).setToastMessage;
+  const getAccessToken = useAuthHeader();
+  const setToast = useContext(ToastContext).setToastMessage;
 
-	const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-	const [allEvents, setAllEvents] = useState<Event[]>([]);
-	const liveEvents = useMemo(
-		() => allEvents.filter((event) => event.isStarted),
-		[allEvents]
-	);
-	const liveAbleEvents = useMemo(
-		() =>
-			allEvents.filter(
-				(e) =>
-					!e.isCompleted &&
-					(e.startTime as number) <= new Date().getTime() + EVENT_START_BUFFER
-			),
-		[allEvents]
-	);
+  const [allEvents, setAllEvents] = useState<Event[]>([]);
+  const liveEvents = useMemo(() => allEvents.filter(event => event.isStarted), [allEvents]);
+  const liveAbleEvents = useMemo(
+    () => allEvents.filter(e => !e.isCompleted && (e.startTime as number) <= new Date().getTime() + EVENT_START_BUFFER),
+    [allEvents]
+  );
 
-	const fetchEvents = async () => {
-		const result: Event[] = (await API.GetEvents()).data;
-		setAllEvents(result);
-		setLoading(false);
-	};
+  const fetchEvents = async () => {
+    const result: Event[] = (await API.GetEvents()).data;
+    setAllEvents(result);
+    setLoading(false);
+  };
 
-	const handleScoreUpdate = async (id: string, score: any) => {
-		await API.UpdateScore(getAccessToken(), id, score);
-		const newEvents = allEvents.map((e) => {
-			if (e._id === id) return { ...e, score: score };
-			else return e;
-		});
-		setAllEvents(newEvents);
-	};
+  const handleScoreUpdate = async (id: string, score: any) => {
+    await API.UpdateScore(getAccessToken(), id, score);
+    const newEvents = allEvents.map(e => {
+      if (e._id === id) return { ...e, score: score };
+      else return e;
+    });
+    setAllEvents(newEvents);
+  };
 
-	useEffect(() => {
-		fetchEvents();
-	}, []);
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
-	const getEventBox = (event: Event, i: number): React.JSX.Element => {
-		switch (event.event) {
-			case EventCatagories.FOOTBALL:
-				return (
-					<FootballEventBox
-						isAdmin
-						onScoreUpdate={(score) => handleScoreUpdate(event._id!, score)}
-						key={i}
-						event={event as FootballEvent}
-					/>
-				);
-			case EventCatagories.CHESS:
-				return (
-					<ChessEventBox
-						isAdmin
-						onScoreUpdate={(score) => handleScoreUpdate(event._id!, score)}
-						key={i}
-						event={event as ChessEvent}
-					/>
-				);
-			case EventCatagories.SQUASH_MEN || EventCatagories.SQUASH_WOMEN:
-				return (
-					<SquashEventBox
-						isAdmin
-						onScoreUpdate={(score) => handleScoreUpdate(event._id!, score)}
-						key={i}
-						event={event as SquashEvent}
-					/>
-				);
-			case EventCatagories.TENNIS_MEN || EventCatagories.TENNIS_WOMEN:
-				return (
-					<TennisEventBox
-						isAdmin
-						onScoreUpdate={(score) => handleScoreUpdate(event._id!, score)}
-						key={i}
-						event={event as TennisEvent}
-					/>
-				);
-			default:
-				return <></>;
-		}
-	};
+  const getEventBox = (event: Event, i: number): React.JSX.Element => {
+    switch (event.event) {
+      case EventCatagories.FOOTBALL:
+        return (
+          <FootballEventBox
+            isAdmin
+            onScoreUpdate={score => handleScoreUpdate(event._id!, score)}
+            key={i}
+            event={event as FootballEvent}
+          />
+        );
+      case EventCatagories.CHESS:
+        return (
+          <ChessEventBox
+            isAdmin
+            onScoreUpdate={score => handleScoreUpdate(event._id!, score)}
+            key={i}
+            event={event as ChessEvent}
+          />
+        );
+      case EventCatagories.SQUASH_MEN || EventCatagories.SQUASH_WOMEN:
+        return (
+          <SquashEventBox
+            isAdmin
+            onScoreUpdate={score => handleScoreUpdate(event._id!, score)}
+            key={i}
+            event={event as SquashEvent}
+          />
+        );
+      case EventCatagories.TENNIS_MEN || EventCatagories.TENNIS_WOMEN:
+        return (
+          <TennisEventBox
+            isAdmin
+            onScoreUpdate={score => handleScoreUpdate(event._id!, score)}
+            key={i}
+            event={event as TennisEvent}
+          />
+        );
+      default:
+        return <></>;
+    }
+  };
 
-	return (
-		<div className="usersContainer">
-			<div className="top" style={{ fontWeight: "600" }}>
-				Live Events
-			</div>
-			<div className="main">
-				{!loading ? (
-					liveEvents && (
-						<section className="liveEvents">
-							{liveEvents.map((event, i) => getEventBox(event, i))}
-						</section>
-					)
-				) : (
-					<>Loading Events Data...</>
-				)}
-				<section className="liveAbleEvents">
-					{liveAbleEvents.length !== 0 ? (
-						liveAbleEvents.map((event, i) => (
-							<div key={i}>
-								{event.event} -{" "}
-								{(event as AthleticsEvent).athleticsEventType
-									? (event as AthleticsEvent).athleticsEventType + " - "
-									: null}
-								{event.title} -{" "}
-								{new Date(event.startTime).toLocaleDateString("en-GB")} - Start
-								Time:{" "}
-								{new Date(event.startTime).toLocaleString("en-US", {
-									hour: "numeric",
-									minute: "numeric",
-									hour12: true,
-								})}{" "}
-								- {event.isStarted ? "Is Live" : "Not Live"}
-								<ul>
-									{event.teams.map((team, i) => (
-										<li key={i}>{team.name} </li>
-									))}
-								</ul>
-								<button
-									className="styledButton"
-									onClick={async () => {
-										if (event.isStarted) {
-											if (
-												(event.endTime as number) - EVENT_START_BUFFER >
-												new Date().getTime()
-											) {
-												setToast("Can't end this event right now!");
-												return;
-											}
-										}
-										try {
-											await API.ToggleEventStatus(getAccessToken(), event._id!);
-											fetchEvents();
-										} catch (error: any) {
-											try {
-												setToast(JSON.parse(error.request.response).message);
-											} catch {
-												setToast("Could not connect with the Server");
-												console.log(error);
-											}
-										}
-									}}
-								>
-									{event.isStarted ? "End Event" : "Go Live"}
-								</button>
-							</div>
-						))
-					) : (
-						<>No Events which can be toggled live!</>
-					)}
-				</section>
-			</div>
-		</div>
-	);
+  return (
+    <div className="usersContainer">
+      <div className="top" style={{ fontWeight: "600" }}>
+        Live Events
+      </div>
+      <div className="main">
+        {!loading ? (
+          liveEvents && <section className="liveEvents">{liveEvents.map((event, i) => getEventBox(event, i))}</section>
+        ) : (
+          <>Loading Events Data...</>
+        )}
+        <section className="liveAbleEvents">
+          {liveAbleEvents.length !== 0 ? (
+            liveAbleEvents.map((event, i) => (
+              <div key={i}>
+                {event.event} -{" "}
+                {(event as AthleticsEvent).athleticsEventType
+                  ? (event as AthleticsEvent).athleticsEventType + " - "
+                  : null}
+                {event.title} - {new Date(event.startTime).toLocaleDateString("en-GB")} - Start Time:{" "}
+                {new Date(event.startTime).toLocaleString("en-US", {
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: true,
+                })}{" "}
+                - {event.isStarted ? "Is Live" : "Not Live"}
+                <ul>
+                  {event.teams.map((team, i) => (
+                    <li key={i}>{team.name} </li>
+                  ))}
+                </ul>
+                <button
+                  className="styledButton"
+                  onClick={async () => {
+                    if (event.isStarted) {
+                      if ((event.endTime as number) - EVENT_START_BUFFER > new Date().getTime()) {
+                        setToast("Can't end this event right now!");
+                        return;
+                      }
+                    }
+                    try {
+                      await API.ToggleEventStatus(getAccessToken(), event._id!);
+                      fetchEvents();
+                    } catch (error: any) {
+                      try {
+                        setToast(JSON.parse(error.request.response).message);
+                      } catch {
+                        setToast("Could not connect with the Server");
+                        console.log(error);
+                      }
+                    }
+                  }}
+                >
+                  {event.isStarted ? "End Event" : "Go Live"}
+                </button>
+              </div>
+            ))
+          ) : (
+            <>No Events which can be toggled live!</>
+          )}
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default EditScores;
---
+++ src/components/AthleticsGamesResultLog.tsx
@@ -0,0 +1,72 @@
+import AthleticsEvent from "../types/AthleticsEvent";
+import { formatEventName } from "../types/EventCategories";
+import { useState } from "react";
+import "./table.css";
+
+interface AthleticsGamesResultLogProps {
+  event: AthleticsEvent;
+}
+
+const AthleticsGamesResultLog: React.FC<AthleticsGamesResultLogProps> = ({ event }) => {
+  const [dialogueOpen, setDialogueOpen] = useState<boolean>(false);
+  return (
+    <>
+      {dialogueOpen && (
+        <div className="dialogue">
+          <div className="dialogue-box">
+            <div className="dialogue-box-head">
+              <h2>
+                {formatEventName(event.athleticsEventType)} {event.title} Results
+              </h2>
+              <div className="dialogue-close" onClick={() => setDialogueOpen(false)}>
+                &times;
+              </div>
+            </div>
+
+            <div className="dialogue-body">
+              <table className="excel-like-table" id="result-table">
+                <thead>
+                  <tr>
+                    <th>Rank</th>
+                    <th>Participant</th>
+                    <th>Team</th>
+                    <th>
+                      {!!event.winner?.participants && !!event.winner?.participants[0].distance ? "Distance" : "Time"}
+                    </th>
+                  </tr>
+                </thead>
+
+                {!!event.winner?.participants && (
+                  <tbody>
+                    {event.winner.participants.map((p, i) => {
+                      console.log(p);
+                      return (
+                        <tr key={i}>
+                          <td>{i + 1}</td>
+                          <td>{p.name}</td>
+                          <td>{p.team}</td>
+                          <td>{p.distance ? p.distance + "m" : p.time}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                )}
+              </table>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="resultLog fjalla athletic-result-log">
+        <span>
+          {formatEventName(event.athleticsEventType)} {event.title} concluded.
+        </span>
+        <div className="tooltip" onClick={() => setDialogueOpen(true)}>
+          View results
+        </div>
+        {/* TODO: Add view results */}
+      </div>
+    </>
+  );
+};
+
+export default AthleticsGamesResultLog;
